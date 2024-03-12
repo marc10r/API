e4bf804ad5cf7256fad5ef76b7bd7166aff010d5
marc <khourymark1@gmail.com>
2024-03-12 21:37:35 +0200

    Adding Students endpoint

--- api.js
+++ api.js
@@ -18,6 +18,11 @@ const PORT = process.env.PORT || 3030;
 app.get('/',(req,res)=>{
     res.send('sarah!!!!')
 })
+
+app.get('/students',(req,res)=>{
+  let data = [{id:54, name:'sophie'},{id:22, name:'jad'},{id:12, name:'ali'}]
+  res.send(data)
+})
 // your code
 
 app.listen(PORT, () => {
